4dca5a4e22b225e5e0dbbb2ea055d343f85491ad
The4thLaw <The4thLaw@users.noreply.github.com>
2016-07-01 18:12:24 +0200

    Fixing format.

--- src/main/java/org/the4thlaw/bm3/PlaylistReader.java
+++ src/main/java/org/the4thlaw/bm3/PlaylistReader.java
@@ -11,40 +11,40 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PlaylistReader implements Closeable, AutoCloseable {
-    private static final Logger LOGGER = LoggerFactory.getLogger(PlaylistReader.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(PlaylistReader.class);
 
-    private final BufferedReader reader;
-    private final File playlistDirectory;
+	private final BufferedReader reader;
+	private final File playlistDirectory;
 
-    public PlaylistReader(File playlist) throws FileNotFoundException {
-        reader = new BufferedReader(new FileReader(playlist));
-        playlistDirectory = playlist.getParentFile();
-    }
+	public PlaylistReader(File playlist) throws FileNotFoundException {
+		reader = new BufferedReader(new FileReader(playlist));
+		playlistDirectory = playlist.getParentFile();
+	}
 
-    public File getEntry() throws IOException {
-        String line;
-        while ((line = reader.readLine()) != null) {
-            if (!line.startsWith("#")) {
-                break;
-            }
-        }
-        if (line == null) {
-            return null;
-        }
+	public File getEntry() throws IOException {
+		String line;
+		while ((line = reader.readLine()) != null) {
+			if (!line.startsWith("#")) {
+				break;
+			}
+		}
+		if (line == null) {
+			return null;
+		}
 
-        File musicFile = new File(line);
-        if (!musicFile.isAbsolute()) {
-            musicFile = new File(playlistDirectory, line);
-        }
-        musicFile = musicFile.getCanonicalFile();
+		File musicFile = new File(line);
+		if (!musicFile.isAbsolute()) {
+			musicFile = new File(playlistDirectory, line);
+		}
+		musicFile = musicFile.getCanonicalFile();
 
-        LOGGER.debug("Found file {}", musicFile);
+		LOGGER.debug("Found file {}", musicFile);
 
-        return musicFile;
-    }
+		return musicFile;
+	}
 
-    @Override
-    public void close() throws IOException {
-        reader.close();
-    }
+	@Override
+	public void close() throws IOException {
+		reader.close();
+	}
 }
